f4fe97cfad88cb6e94c1fa705c5f72f24eae14c7
khandkaramin1 <aminit14@gmail.com>
2023-02-04 16:51:26 -0500

    first commit

---
+++ src/com/upskill/java_1/AccessModifier.java
@@ -0,0 +1,11 @@
+package com.upskill.java_1;
+
+public class AccessModifier {
+
+	String name = "Amin";    //Public - ANy Class Can Access
+	Private int age = 31;    //Private - Only Same class can access
+	Protected int ssn = 251487598;  //Protected - Classes in the same package and subclass can access
+	
+	string address = "Thomas ST";  //Default - Class in same package can access
+	
+}
---
+++ src/com/upskill/java_1/DataTypes.java
@@ -0,0 +1,30 @@
+package com.upskill.java_1;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World, This is Amin");
+		
+		int a = 5;
+		int b = 10;
+		int sum = a+b;
+		
+		System.out.println(sum);
+		
+		//String City = "NewYork";
+		
+		/*System.out.println(City);
+		
+		boolean isGood = true;
+		System.out.println(isGood);*/
+		
+	}
+
+}
+
+
+
+
+
+
